deb68578fcdf3b84f985c1310a2a15abafde56e0
diegonmarcos <diegonmarcos@gmail.com>
2025-03-12 12:01:50 +0100

    fdf stable

--- C2_fdf/2_SRC/inc/fdf.h
+++ C2_fdf/2_SRC/inc/fdf.h
@@ -6,7 +6,7 @@
 /*   By: dinepomu <dinepomu@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 15:40:13 by dinepomu          #+#    #+#             */
-/*   Updated: 2025/03/12 11:43:35 by dinepomu         ###   ########.fr       */
+/*   Updated: 2025/03/12 12:01:28 by dinepomu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,10 +51,15 @@
 
 // ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓	//
 /*#####################################################################	*/
-/* 0. MYLIBX															*/
+/* 5. MYLIBX															*/
 /*#####################################################################	*/
 /* ***************************************************************** */
-/* 0.1 Draw Frame/Line												*/
+/* 5.0 Transformation												 */
+/* ***************************************************************** */
+t_point	matrix3_multp(float matrix[3][3], t_point point);
+
+/* ***************************************************************** */
+/* 5.1 Render/Frame														*/
 /* ***************************************************************** */
 void	mydrawframe(t_meta *meta, t_point *wire);
 void	mydrawframe_row_lines(t_point *point, t_meta *meta, \
@@ -64,25 +69,19 @@ int		myputpixel_check(t_meta *meta, t_point pixel);
 void	myputpixel(char *buffer, int endian, int color, int alpha);
 
 /* ***************************************************************** */
-/* 0.1 Draw Circle													 */
+/* 5.1 Render/Circle													 */
 /* ***************************************************************** */
 void	mydrawcircle(t_meta *meta, t_point *map_points);
 void	draw_dot(t_meta *meta, t_point point, int radius);
 void	dot_util(t_meta *meta, t_point pixel, t_point point, int coord);
 
 /* ***************************************************************** */
-/* 3.4 Draw Utils													 */
+/* 5.2 Utils														 */
 /* ***************************************************************** */
 int		valid_pixel(t_point pixel);
 int		limits(t_point *points, int len);
 int		color_convert_depth(t_meta *meta, int color);
-
-/* ***************************************************************** */
-/* 3.4 Draw Utils/Colorize											 */
-/* ***************************************************************** */
-void	load_color(int max, int min, t_point *point, t_colors colors);
-void	colorize(t_map *map, t_meta *meta);
-int		gradient(int startcolor, int endcolor, int len, int pix);
+void	copy_map(t_point *src, t_point *dst, int len);
 
 // ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓	//
 /*#####################################################################	*/
@@ -95,7 +94,6 @@ void	map_load(t_map *map, char *path, t_meta *meta);
 void	map_ini(t_map *map, int total);
 void	map_ini_colors(t_map *map);
 void	map_size(t_map *map);
-void	copy_map(t_point *src, t_point *dst, int len);
 void	map_checker(char c);
 
 /* ***************************************************************** */
@@ -106,42 +104,63 @@ int		load_points(char *line, t_map *map, int numline);
 int		valid_point(char *value);
 int		has_hexcolors(char *line);
 
-// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓	//
-int		map_pipeline(t_meta *meta, int fit);
+/* ***************************************************************** */
+/* 1.2 Load Collors													 */
+/* ***************************************************************** */
+void	colorize(t_map *map, t_meta *meta);
+void	load_color(int max, int min, t_point *point, t_colors colors);
+int		gradient(int startcolor, int endcolor, int len, int pix);
 
+// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓	//
 /*#####################################################################	*/
-/* 2. MAP TRANSFORMATION												*/
+/* 2. MAP RENDER PIPELINE												*/
+/*#####################################################################	*/
+/* ***************************************************************** */
+/* 2.0 Pipeline														 */
+/* ***************************************************************** */
+int		map_pipeline(t_meta *meta, int fit);
+void	map_pipeline_recursive(t_meta *meta);
+
+/* ***************************************************************** */
+/* 2.1 Events														 */
+/* ***************************************************************** */
+int		key_press(int key, void *param);
+int		mouse_press(int button, int x, int y, void *param);
+int		mouse_move(int x, int y, void *param);
+
+// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓	//
+/*#####################################################################	*/
+/* 3. MAP TRANSFORMATION												*/
 /*#####################################################################	*/
 void	map_transformation(t_meta *meta, t_point *map_points);
 
 /* ***************************************************************** */
 /* 2. Map Transformation											 */
 /* ***************************************************************** */
-void	z_division(t_point *map_points, float divisor, int len);
-void	bending(t_point *points, int len, float range);
 void	rotate_x(t_point *points, t_point *projection, float ang, int len);
 void	rotate_y(t_point *points, t_point *projection, float ang, int len);
 void	rotate_z(t_point *points, t_point *projection, float ang, int len);
 void	ortogonal_projection(t_point *points, t_point *projection, int len);
-t_point	matrix3_multp(float matrix[3][3], t_point point);
+void	z_division(t_point *map_points, float divisor, int len);
+void	bending(t_point *points, int len, float range);
 void	scale(t_point *points, int scale, int len);
 void	traslate(t_point *points, t_point move, int len);
 void	go_polar(t_map *map);
 
 // ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓	//
 /*#####################################################################	*/
-/* 3. MAP RENDER															*/
+/* 4. MAP RENDER															*/
 /*#####################################################################	*/
 void	map_render(t_meta *meta, t_point *map_points);
 
 /* ***************************************************************** */
-/* 3.1 Render											 */
+/* 4.1 Render											 */
 /* ***************************************************************** */
 void	render_background(t_meta *meta, int backcolor, int menucolor);
 void	fit_to_scale(t_meta *meta, t_point *map_points, int fit);
 
 /* ***************************************************************** */
-/* 3.2 Menu															 */
+/* 4.2 Menu															 */
 /* ***************************************************************** */
 void	render_menu(t_meta *meta);
 void	draw_info(t_meta *meta);
@@ -150,7 +169,7 @@ void	draw_mapinfo(t_meta *meta);
 void	draw_colorscheme(t_meta *meta);
 
 /* ***************************************************************** */
-/* 3.3 Views Features/Bonus											 */
+/* 4.3 Views Features/Bonus											 */
 /* ***************************************************************** */
 void	isometric(t_map *map);
 void	parallel(t_map *map);
@@ -160,45 +179,35 @@ void	shadow(t_point *points, int len);
 
 // ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓	//
 /*#####################################################################	*/
-/* 4. SYSTEM															*/
+/* 6. DATA | UTILS | KEYS												*/
 /*#####################################################################	*/
 /* ***************************************************************** */
-/* 4.1 System														 */
+/* 6.0 Variables Initialization										 */
 /* ***************************************************************** */
 void	vars_init_x11(t_meta *meta);
 void	vars_init_map(t_meta *meta);
-void	map_pipeline_recursive(t_meta *meta);
 
 /* ***************************************************************** */
-/* 4.2 Control Keys													 */
+/* 6.1 Control Keys													 */
 /* ***************************************************************** */
-//# include "keycodes.h"
-int		key_press(int key, void *param);
 int		key_release(int key, void *param);
 void	control_keys1(int key, t_meta *meta);
 void	control_keys2(int key, t_meta *meta);
 void	control_keys3(int key, t_meta *meta);
 
 /* ***************************************************************** */
-/* 4.3 Control Mouse												 */
+/* 6.2 Control Keys Utils											 */
 /* ***************************************************************** */
-int		mouse_move(int x, int y, void *param);
 int		mouse_release(int button, int x, int y, void *param);
-int		mouse_press(int button, int x, int y, void *param);
-
-/* ***************************************************************** */
-/* 4.4 Control Utils												 */
-/* ***************************************************************** */
 void	control_colorscheme(int key, t_map *map, t_meta *meta);
 void	angle_control(int key, t_meta *meta);
 void	angle(float *ang, float value);
 int		auto_rotation_1(void *param);
 int		auto_rotation_2(void *param);
 
-// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓	//
-/*#####################################################################	*/
-/* 4. UTILS															*/
-/*#####################################################################	*/
+/* ***************************************************************** */
+/* 6.3 Utils												 		*/
+/* ***************************************************************** */
 int		halt_exit_program(void *param);
 void	terminate(char *s);
 void	terminate1(char *s, t_meta *meta);
